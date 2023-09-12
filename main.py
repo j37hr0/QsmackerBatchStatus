844cf5c26d5d9ff9f75cead91bc89b7be1064960
jethro <jethro.cotton3@gmail.com>
2023-09-12 14:56:28 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pymssql
+import pandas
+from decouple import config
+
+sqlserver = config('SQLSERVER')
+sqluser = config('SQLUSER')
+sqlpass = config('SQLPASS')
+
+batchName = input("Enter the batch name (exact name works better): ")
+openBatchIDs = []
+completeBatchIDs = []
+#Added tag 103337 #2
+#Added tag 103337 #1
+#96000000006021FF 88821 #2
+#96000000006021FF 88821 #1
+#88867 - Pin Reactivation for failed machines at GLS PnP Eastport #1
+try:
+    connection = pymssql.connect(sqlserver, sqluser, sqlpass, "Qsmacker")
+    cursor = connection.cursor(as_dict=True)
+    cursor.execute(f"exec GetBatchStatus '{batchName}'")
+    for row in cursor:
+        print(row["batchStatusId"])
+        if row["batchStatusId"] == 1:
+            openBatchIDs.append(row["id"])
+        elif row["batchStatusID"] == 3:
+            completeBatchIDs.append({row["id"], row["refNo"]})
+    print(openBatchIDs)
+    print(completeBatchIDs)
+except Exception as e:
+    print("couldn't connect", e)
